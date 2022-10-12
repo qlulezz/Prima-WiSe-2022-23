4b171032109023d4d290634b014514e621a2d808
Henry <HenryCraft@gmx.de>
2022-10-12 22:45:51 +0200

    Walk animation using NodeSprite

--- Mario/Script/Build/Script.js
+++ Mario/Script/Build/Script.js
@@ -1,1 +1,85 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    function start(_event) {
+        viewport = _event.detail;
+        hndLoad(_event);
+        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.simulate();  // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+    const framerate = 8;
+    let spriteNode;
+    async function hndLoad(_event) {
+        let root = new ƒ.Node("root");
+        let imgSpriteSheet = new ƒ.TextureImage();
+        await imgSpriteSheet.load("./Images/characters.gif");
+        let coat = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+        let animation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+        animation.generateByGrid(ƒ.Rectangle.GET(297, 1, 16, 32), 3, 16, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(17));
+        spriteNode = new ƒAid.NodeSprite("Sprite");
+        spriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        spriteNode.setAnimation(animation);
+        spriteNode.setFrameDirection(1);
+        spriteNode.mtxLocal.translateY(-1);
+        spriteNode.framerate = framerate;
+        root.addChild(spriteNode);
+        // Replace temp cam and viewport with already initialized scene
+        let cmpCamera = new ƒ.ComponentCamera();
+        cmpCamera.mtxPivot.translateZ(5);
+        cmpCamera.mtxPivot.rotateY(180);
+        const canvas = document.querySelector("canvas");
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", root, cmpCamera, canvas);
+        viewport.camera.clrBackground = ƒ.Color.CSS("Black");
+        viewport.draw();
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, framerate);
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Mario/Script/Source/Main.ts
+++ Mario/Script/Source/Main.ts
@@ -1,5 +1,6 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
@@ -8,7 +9,7 @@ namespace Script {
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    hndLoad(_event);
     // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
@@ -16,5 +17,44 @@ namespace Script {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+  }
+
+  const framerate = 8;
+
+  let spriteNode: ƒAid.NodeSprite;
+
+  async function hndLoad(_event: Event): Promise<void> {
+    let root: ƒ.Node = new ƒ.Node("root");
+
+    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgSpriteSheet.load("./Images/characters.gif");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+
+    let animation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+    animation.generateByGrid(ƒ.Rectangle.GET(297, 1, 16, 32), 3, 16, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(17));
+
+    spriteNode = new ƒAid.NodeSprite("Sprite");
+    spriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    spriteNode.setAnimation(animation);
+    spriteNode.setFrameDirection(1);
+    spriteNode.mtxLocal.translateY(-1);
+    spriteNode.framerate = framerate;
+
+    root.addChild(spriteNode);
+
+    // Replace temp cam and viewport with already initialized scene
+    let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    cmpCamera.mtxPivot.translateZ(5);
+    cmpCamera.mtxPivot.rotateY(180);
+
+    const canvas: HTMLCanvasElement = document.querySelector("canvas");
+
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", root, cmpCamera, canvas);
+    viewport.camera.clrBackground = ƒ.Color.CSS("Black");
+    viewport.draw();
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, framerate);
   }
 }
